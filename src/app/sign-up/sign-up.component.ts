a5e41d0a4672071e3422467e1ff3d8c1fdcaf461
BobBieNeno <161856266+BobBieNeno@users.noreply.github.com>
2024-03-04 15:14:12 +0700

    Update sign-up.component.ts

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,20 +1,47 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar'; 
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input'; 
 import {MatIconModule} from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
+import { lastValueFrom } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { ServiceService } from '../service.service';
+// import { MatFormField } from '@angular/material/input';
+// import { MatFormFieldControl } from '@angular/material/form-field';
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [ RouterModule ,MatToolbarModule,MatButtonModule,MatInputModule,MatIconModule,MatCardModule],
+  imports: [HttpClientModule,FormsModule, CommonModule,RouterModule ,MatToolbarModule,MatButtonModule,MatInputModule,MatIconModule,MatCardModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+  id:any;
+email: any='' ;
+pass: any='' ;
+name: any='';
 
-  constructor(){
+  constructor(private http:HttpClient,private service:ServiceService){
     
   }
+  async sigup(){
+  const body = {
+    name: this.name,
+    type:0,
+    email: this.email,
+    pass: this.pass
+  }
+  console.log("body",body);
+  
+  // await this.service.signUp(body);
+}
+  async profileid(){
+   this.id = await this.service.get_new_user();
+   this.service.id = this.id;
+}
 }
