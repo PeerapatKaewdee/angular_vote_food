c5186bedf5ec2a6ed54e33b45afd9103b1070b57
BobBieNeno <161856266+BobBieNeno@users.noreply.github.com>
2024-03-04 11:32:29 +0700

    Update vote.component.ts
    11;32

--- src/app/vote/vote.component.ts
+++ src/app/vote/vote.component.ts
@@ -27,7 +27,7 @@ export class VoteComponent {
 
    constructor(private http: HttpClient, private service: ServiceService,private ActivatedRoute:ActivatedRoute){
 
-    this.vote();
+    // this.vote();
   
     this.id = this.service.id;
     console.log("service.id",this.service.id);
@@ -48,19 +48,19 @@ export class VoteComponent {
 
     console.log(this.user);
   }
-  async vote(){
-    this.img = await this.service.get_img_ran();
-    this.img1 = this.img[0];
-    this.img2 = this.img[1];
-    console.log("img Ran",this.img);
+  // async vote(){
+  //   this.img = await this.service.get_img_ran();
+  //   this.img1 = this.img[0];
+  //   this.img2 = this.img[1];
+  //   console.log("img Ran",this.img);
     
-  }
-  async score( fid :any){
-    this.foods_score = 999;
-      console.log("fid",fid);
-      await this.service.upscore(fid, this.foods_score);
+  // }
+  // async score( fid :any){
+  //   this.foods_score = 999;
+  //     console.log("fid",fid);
+  //     await this.service.upscore(fid, this.foods_score);
       
-  }
+  // }
 
 
 }
