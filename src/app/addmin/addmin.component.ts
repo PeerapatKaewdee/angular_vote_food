1c01431a2545a9224c5fd1b7c0c720796847f389
PeerapatKaewdee <136581789+PeerapatKaewdee@users.noreply.github.com>
2024-03-05 01:12:45 +0700

    Revert "Revert "admin view""
    This reverts commit de9ed16bd183642b77b5fd74bd232644593fec26.

---
+++ src/app/addmin/addmin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addmin',
+  standalone: true,
+  imports: [],
+  templateUrl: './addmin.component.html',
+  styleUrl: './addmin.component.css'
+})
+export class AddminComponent {
+
+}
