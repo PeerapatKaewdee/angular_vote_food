6a04a1ffc7235f035781b57fea7f52e69bb05989
PeerapatKaewdee <136581789+PeerapatKaewdee@users.noreply.github.com>
2024-03-12 22:38:57 +0700

    Update graph.component.ts

--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -1,6 +1,5 @@
-import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Chart } from 'chart.js';
-
 import { RouterModule } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar'; 
 import {MatButtonModule} from '@angular/material/button';
@@ -17,47 +16,12 @@ import { MatFormFieldModule} from '@angular/material/form-field';
   templateUrl: './graph.component.html',
   styleUrl: './graph.component.css'
 })
-export class ChartComponent implements AfterViewInit {
-  @ViewChild('myChart') myChart: ElementRef;
+export class GraphComponent {
 
-  ngAfterViewInit() {
-    const ctx: CanvasRenderingContext2D = this.myChart.nativeElement.getContext('2d');
-    
-    new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
-      }
-    });
+  goBack() {
+    // Implementing window.back to go back to the previous location
+    window.history.back();
   }
+  
+
 }
