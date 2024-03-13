a3a76d4372049bc9b975b84e230d8cc14370ccf3
BobBieNeno <161856266+BobBieNeno@users.noreply.github.com>
2024-03-13 17:19:48 +0700

    Update profile.component.ts

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ServiceService } from '../service.service';
@@ -28,13 +28,15 @@ export class ProfileComponent {
   img:any;
   imgUser: any ='';
   selectedFile: File | null = null;
-  constructor(private http: HttpClient, private service: ServiceService,private ActivatedRoute :ActivatedRoute) {
-
+  constructor(private http: HttpClient, private service: ServiceService,private ActivatedRoute :ActivatedRoute, private router : Router) {
+    this.id = this.service.id;
+    console.log(this.id);
+    
   // this.get_img();    
   }
   
   async ngOnInit(): Promise<void> {
-    this.id = this.ActivatedRoute.snapshot.paramMap.get('uid') || ' ';
+    // this.id = this.ActivatedRoute.snapshot.paramMap.get('uid') || ' ';
 
     console.log("uid",this.id);
     this.service.id = this.id;
@@ -63,26 +65,42 @@ export class ProfileComponent {
     formData.append('file', event);
 
   }
-  uploadImage() {
-    if (this.selectedFile) {
-        const formData = new FormData();
-        formData.append('file', this.selectedFile);
-    console.log("selectedFile2", this.selectedFile);
-
-        // console.log("formData", formData.append);
-
-        // Here you can make an HTTP request to upload the file using formData
-    }
-}
-
-onFileSelected(event: any): void {
+  
+  onFileSelected(event: any): void {
     this.selectedFile = event.target.files[0];
     console.log("selectedFile1", this.selectedFile);
+    this.uploadImage()
     // Optionally, you can call uploadImage() here if you want to upload the file immediately after selection
+  }
+  async uploadImage() {
+    if (this.selectedFile) {
+      const formData = new FormData();
+      formData.append('file', this.selectedFile);
+      formData.append('uid', this.id);
+
+      console.log("formData : 123", formData);
+
+      // รอให้การเรียก post_upProfile() เสร็จสิ้นและรับข้อมูลที่ส่งกลับมา
+      const response = await this.service.post_upProfile(formData);
+      console.log("Response from server:", response);
+
+      console.log("selectedFile2", this.selectedFile);
+
+      // ทำงานอื่นๆที่ต้องการทำหลังจากการอัปโหลดไฟล์
+  }
 }
-
-
-
+ranking(){
+  this.service = this.id;
+  this.router.navigateByUrl('/ranking/');
+}
+  
+vote(){
+  this.service = this.id;
+  this.router.navigateByUrl('/vote/');
+}
+  
+  
+  
   
 
 }
