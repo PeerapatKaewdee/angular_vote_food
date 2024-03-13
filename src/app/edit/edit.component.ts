3f3a7a4ba0db41788ddecda8a1814fbecb5f40ed
BobBieNeno <161856266+BobBieNeno@users.noreply.github.com>
2024-03-13 17:41:37 +0700

    Update edit.component.ts

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar'; 
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input'; 
@@ -7,6 +7,9 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule} from '@angular/material/form-field';
+import { HttpClient } from '@angular/common/http';
+import { ServiceService } from '../service.service';
+import e from 'express';
 @Component({
   selector: 'app-edit',
   standalone: true,
@@ -14,12 +17,36 @@ import { MatFormFieldModule} from '@angular/material/form-field';
   templateUrl: './edit.component.html',
   styleUrl: './edit.component.css'
 })
+
 export class EditComponent {
 
+id: any;
 
+  constructor(private http: HttpClient, private service: ServiceService, private router : Router) {
+    this.id = this.service.id;
+    console.log(this.id);
+    
+  // this.get_img();    
+  }
   goBack() {
     // Implementing window.back to go back to the previous location
     window.history.back();
   }
+  profile(){
+    this.service.id = this.id;
+    this.router.navigateByUrl('/profile/');
+  }
+  confrim(name:any,lastname:any,email:any,pass:any) {
+    console.log("name",name);
+    console.log("lastName",lastname);
+    console.log("email",email);
+    console.log("pass",pass);
+    // console.log("file",file);
+    
+    throw new Error('Method not implemented.');
+    }
+    vote(){
+
+    }
 
 }
