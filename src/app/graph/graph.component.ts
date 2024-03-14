2209d92d372c6ffe2684f946343fb18cde5919da
PeerapatKaewdee <136581789+PeerapatKaewdee@users.noreply.github.com>
2024-03-14 18:31:26 +0700

    Update graph.component.ts

--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 // import { Chart } from 'chart.js';
+import { ChartModule } from 'primeng/chart';
 import { RouterModule } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar'; 
 import {MatButtonModule} from '@angular/material/button';
@@ -9,19 +10,34 @@ import {MatCardModule} from '@angular/material/card';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule} from '@angular/material/form-field';
 
+
+declare var Chart: any;
+
 @Component({
   selector: 'app-graph',
   standalone: true,
-  imports: [ RouterModule ,MatFormFieldModule,MatToolbarModule,MatButtonModule,MatInputModule,MatIconModule,MatCardModule, FormsModule],
+  imports: [ChartModule,RouterModule ,MatFormFieldModule,MatToolbarModule,MatButtonModule,MatInputModule,MatIconModule,MatCardModule, FormsModule],
   templateUrl: './graph.component.html',
   styleUrl: './graph.component.css'
 })
-export class GraphComponent {
+export class GraphComponent implements OnInit {
+  data : any;
+  constructor() { }
 
-  goBack() {
-    // Implementing window.back to go back to the previous location
-    window.history.back();
+  ngOnInit(){
+    this.data = {
+      labels : ["Q1","Q2","Q3","Q4"],
+      datasets :[{
+        label : "first dataset",
+        data : [10,20,30,41],
+        backgroundColor :['green'],
+        borderColor :['Black'],
+        borderWidth: 1
+      },
+    ]
+    }
   }
-  
 
-}
+ 
+  }
+
