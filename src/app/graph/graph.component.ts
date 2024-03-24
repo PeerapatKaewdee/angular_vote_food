14ac32e98a4a7541523f8dbde4491d4833ee356a
BobBieNeno <161856266+BobBieNeno@users.noreply.github.com>
2024-03-25 02:15:29 +0700

    Update graph.component.ts

--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -9,7 +9,8 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule} from '@angular/material/form-field';
-
+import { Router } from '@angular/router';
+import { ServiceService } from '../service.service';
 
 declare var Chart: any;
 
@@ -21,23 +22,43 @@ declare var Chart: any;
   styleUrl: './graph.component.css'
 })
 export class GraphComponent implements OnInit {
-  data : any;
-  constructor() { }
+  img : any;
+  day1 : any;
+  day2 : any;
+  day3 : any;
+  day4 : any;
+  day5 : any;
+  day6 : any;
+  day7 : any;
+  id:any;
+  constructor(private router: Router,private service:ServiceService) {
+    this.id = this.service.id;
+   }
 
-  ngOnInit(){
-    this.data = {
-      labels : ["Q1","Q2","Q3","Q4"],
-      datasets :[{
-        label : "first dataset",
-        data : [10,20,30,41],
-        backgroundColor :['green'],
-        borderColor :['Black'],
-        borderWidth: 1
-      },
-    ]
-    }
+  async ngOnInit(){
+
+   this.day1 = await this.service.get_rank_day1();
+   this.day2 = await this.service.get_rank_day2();
+   this.day3 = await this.service.get_rank_day3();
+   this.day4 = await this.service.get_rank_day4();
+   this.day5 = await this.service.get_rank_day5();
+   this.day6 = await this.service.get_rank_day6();
+   this.day7 = await this.service.get_rank_day7();
+   this.img = {
+    labels : ["Q1","Q2","Q3","Q4","Q5","Q6","Q7"],
+    datasets :[{
+      label : "first dataset",
+      data : [10,20,0,0,0,0,0],
+      backgroundColor :['green'],
+      borderColor :['Black'],
+      borderWidth: 1
+    },
+  ]
   }
-
- 
+  }
+  profile(){
+    this.service.id = this.id;
+    this.router.navigateByUrl('/profile/');
+  }
   }
 
